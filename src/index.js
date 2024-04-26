d63334625939432f6fb47192bd60363dd398961a
Ali Raza <99924946+ARaza-13@users.noreply.github.com>
2024-04-26 18:48:38 -0400

    Created an instance of the discord bot and assigned intents to access certain events

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+const { Client, IntentsBitField } = require("discord.js");
 
+// create a bot instance
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers, // bot will have access to members inside the server
+    IntentsBitField.Flags.GuildMessages, // bot will have access to messages inside the server
+    IntentsBitField.Flags.MessageContent, // bot will be able to read messages inside the server
+  ],
+});
