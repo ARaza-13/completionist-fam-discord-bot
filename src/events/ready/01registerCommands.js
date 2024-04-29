e32b9b2b5493fdbae2b4c96fe28978548d144c50
Ali Raza <99924946+ARaza-13@users.noreply.github.com>
2024-04-29 18:16:36 -0400

    Created a module to retrieve the application commands

--- src/events/ready/01registerCommands.js
+++ src/events/ready/01registerCommands.js
@@ -1,6 +1,15 @@
 const { testServer } = require("../../../config.json");
+const getApplicationCommands = require("../../utils/getApplicationCommands");
 const getLocalCommands = require("../../utils/getLocalCommands");
 
-module.exports = (client) => {
-  const localCommands = getLocalCommands();
+module.exports = async (client) => {
+  try {
+    const localCommands = getLocalCommands();
+    const applicationCommands = await getApplicationCommands(
+      client,
+      testServer,
+    );
+  } catch (error) {
+    console.log(`There was an error: ${error.stack}`);
+  }
 };
---
+++ src/utils/getApplicationCommands.js
@@ -0,0 +1,16 @@
+module.exports = async (client, guildId) => {
+  let applicationCommands;
+
+  // if guildId exists, then we get the commands in that specific guild
+  // and assign them to the application commands
+  if (guildId) {
+    const guild = await client.guilds.fetch(guildId);
+    applicationCommands = guild.commands;
+  } else {
+    // bot will fetch the global commands
+    applicationCommands = await client.application.commands;
+  }
+
+  await applicationCommands.fetch(); // commands need to be fetched at once
+  return applicationCommands;
+};
