d39b00da899c8dbf781628075784ba742ae01acc
Ali Raza <99924946+ARaza-13@users.noreply.github.com>
2024-04-29 15:22:39 -0400

    Imported all event files into the event handler module to extract/run their respective functions

--- src/handlers/eventHandler.js
+++ src/handlers/eventHandler.js
@@ -1,3 +1,25 @@
+const path = require("path");
+const getAllFiles = require("../utils/getAllFiles");
+
 module.exports = (client) => {
   // get all folders inside the 'events' folder
+  const eventFolders = getAllFiles(path.join(__dirname, "..", "events"), true);
+
+  // import all files inside each respective event folder
+  for (const eventFolder of eventFolders) {
+    const eventFiles = getAllFiles(eventFolder);
+    eventFiles.sort((a, b) => a > b); // sorts the files in order
+
+    // get the name of the event based on the folder name
+    const eventName = eventFolder.replace(/\\/g, "/").split("/").pop(); // gets the last array element, which will be the folder name
+
+    // add an event listener and pass in all the event files
+    // loop through all eventFiles and extract the function from each file
+    client.on(eventName, async (arg) => {
+      for (const eventFile of eventFiles) {
+        const eventFunction = require(eventFile);
+        await eventFunction(client, arg); // run the function inside the respective event file
+      }
+    });
+  }
 };
